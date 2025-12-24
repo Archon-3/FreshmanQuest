f4e8dc8d93088db9e3670bb0d64ca53401f78f34
abredagn <abredagn@gmail.com>
2025-12-24 08:02:41 +0300

    [FreshmanQuest] cafteria improvement

--- python/game/ui/cafeteria.py
+++ python/game/ui/cafeteria.py
@@ -18,9 +18,17 @@ def build_popup(h):
             s.flags['ateMeal'] = True
             h['setEnergy'](100)
             h['addXP'](5)
+            h['addStress'](-5)  # Eating reduces stress
+            h['addReputation'](2)  # Socializing at cafeteria
             h['completeQuest']('eatMeal')
-            h['toast']('Meal eaten (+5 XP, energy 100)')
+            h['toast']('Meal eaten (+5 XP, Energy 100, -5 Stress, +2 Reputation)')
             h['rebuild']()
+        elif s.flags['mealCoupon'] and s.energy < 80:
+            # Can eat again if energy is low (but no quest completion)
+            h['setEnergy'](min(100, s.energy + 30))
+            h['addStress'](-3)
+            h['addReputation'](1)
+            h['toast']('Meal eaten (Energy +30, -3 Stress, +1 Reputation)')
 
     p.add_button(
         "Get Meal Coupon",
